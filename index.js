ec36115248256697d980dc9e396bd45e1cdb06ea
Bowen <z.bowen66@gmail.com>
2020-02-06 20:58:09 +0800

    add redis

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
-const Koa = require('koa');
-const chalk = require('chalk');
+const Koa = require("koa");
+const chalk = require("chalk");
+const redis = require("redis");
+const { promisify } = require("util");
+
+const redisClient = redis.createClient();
+const multi = redisClient.multi();
+const exec = promisify(multi.exec.bind(multi));
+
 const app = new Koa();
-console.log('test ENV A=', process.env.A);
+
 console.log(process.argv);
 
 // logger
-
 app.use(async (ctx, next) => {
   await next();
-  const rt = ctx.response.get('X-Response-Time');
+  const rt = ctx.response.get("X-Response-Time");
   console.log(`${ctx.method} ${ctx.ip} ${ctx.url}`, chalk.green(rt));
 });
 
 // x-response-time
-
 app.use(async (ctx, next) => {
   const start = Date.now();
   await next();
   const ms = Date.now() - start;
-  ctx.set('X-Response-Time', `${ms}ms`);
+  ctx.set("X-Response-Time", `${ms}ms`);
 });
 
 // response
-
 app.use(async ctx => {
-  ctx.body = 'Hello World\n';
+  multi.incr("count");
+  const count = await exec();
+  ctx.body = `Hello, you have been saw me ${count} times\n`;
 });
 
 app.listen(3000, () => {
-	console.log('app listen at port 3000\n');
+  console.log("app listen at port 3000\n");
 });
