5d98a1daa3815539d753a3998f44f031d62ecf7f
Bowen <z.bowen66@gmail.com>
2020-02-11 22:57:54 +0800

    perf

--- index.js
+++ index.js
@@ -10,29 +10,21 @@ const incrAsync = promisify(redisClient.incr.bind(redisClient));
 
 const app = new Koa();
 
-console.log(process.argv);
-
 // logger
-app.use(async (ctx, next) => {
-  await next();
-  const rt = ctx.response.get("X-Response-Time");
-  console.log(`${ctx.method} ${ctx.ip} ${ctx.url}`, chalk.green(rt));
-});
-
-// x-response-time
 app.use(async (ctx, next) => {
   const start = Date.now();
   await next();
   const ms = Date.now() - start;
   ctx.set("X-Response-Time", `${ms}ms`);
+  console.log(`${ctx.method} ${ctx.ip} ${ctx.url}`, chalk.green(`${ms}ms`));
 });
 
 // response
 app.use(async ctx => {
   const count = await incrAsync("count");
-  ctx.body = `Hello, you have met me ${count} times\n`;
+  ctx.body = `Hello, you have met me ${count} times`;
 });
 
 app.listen(3000, () => {
-  console.log("app listen at port 3000\n");
+  console.log("listened 3000\n");
 });
