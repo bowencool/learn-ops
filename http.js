620aa4257970af0167f1fabcab6e9899f25546d6
Bowen <z.bowen66@gmail.com>
2020-02-14 14:46:05 +0800

    心跳

--- http.js
+++ http.js
@@ -1,14 +1,11 @@
 const http = require("http");
 const fs = require("fs");
 
-const leak = [];
-
 http
   .createServer((req, res) => {
-    console.log(req.url);
-    const content = fs.readFileSync("./test.html");
-    // console.log(window.location.href);
-    leak.push(content);
-    res.end(content);
+    console.log(window.location);
+    fs.createReadStream("./test.html").pipe(res);
   })
   .listen(3000, () => console.log("listened 3000"));
+
+// while (true) {}
--- main.js
+++ main.js
@@ -1,31 +1,67 @@
 const cluster = require("cluster");
 const os = require("os");
 
+const MB = 2 ** 20;
+// const LEN = 2;
+const LEN = os.cpus().length / 2;
+
 if (cluster.isMaster) {
-  for (let i = 0, len = os.cpus().length / 2; i < len; i++) {
-    cluster.fork();
+  function fork() {
+    const worker = cluster.fork();
+    let missed = 0;
+    // 开启心跳
+    const timer = setInterval(() => {
+      worker.send("ping");
+      console.log("ping", worker.process.pid);
+      missed++;
+      if (missed >= 3) {
+        process.kill(worker.process.pid);
+      }
+    }, 1000);
+
+    // 停止心跳，自动重启
+    worker.on("exit", (code, signal) => {
+      clearInterval(timer);
+      console.warn("worker %d died. restarting...", worker.process.pid);
+      setTimeout(() => {
+        fork();
+      }, 3000);
+    });
+
+    worker.on("message", msg => {
+      console.log(msg);
+      missed--;
+    });
   }
-  cluster.on("exit", (worker, code, signal) => {
-    console.warn(
-      "worker %d died (%s). restarting...",
-      worker.process.pid,
-      signal || code
-    );
-    setTimeout(() => {
-      cluster.fork();
-    }, 3000);
-  });
+
+  for (let i = 0; i < LEN; i++) {
+    fork();
+  }
+  // 自动重启
+  // cluster.on("exit", (worker, code, signal) => {
+  //   console.warn("worker %d died. restarting...", worker.process.pid);
+  //   setTimeout(() => {
+  //     fork();
+  //   }, 3000);
+  // });
 } else {
   require("./http");
+  // 异常监控
   process.on("uncaughtException", err => {
     console.error("uncaughtException", err.message);
     process.exit(1);
   });
+  // 心跳
+  process.on("message", msg => {
+    if (msg === "ping") {
+      process.send("pong");
+    }
+  });
   // 内存监控
   setInterval(() => {
     const mu = process.memoryUsage().rss;
-    if (mu > 5000000 * 8) {
-      console.log("memory", mu);
+    console.log("🌗", process.pid, `${(mu / MB).toFixed(2)}MB`);
+    if (mu > 100 * MB) {
       process.exit(1);
     }
   }, 5000);
